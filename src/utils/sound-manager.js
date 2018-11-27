150b4a90829f4d5c5ed3ae82d6fb2071c84f5324
Rafael Plugge <rafael.plugge@email.com>
2018-11-27 16:17:29 +0000

    Added ability to define and change audio for specific songs.

--- src/utils/sound-manager.js
+++ src/utils/sound-manager.js
@@ -30,11 +30,17 @@ class SoundManager {
     this.audioContext = null;
 
     /**
+     * Will contain a mapping of song names to their volume nodes.
+     * @type {{"sound-name": GainNode}}
+     */
+    this.volumeNodes = {};
+
+    /**
      * Defined as our volume node that controls
      * the volume for all playing sounds.
      * @type {GainNode}
      */
-    this.volumeNode = null;
+    this.globalVolumeNode = null;
 
     /**
      * Represents our master volume.
@@ -53,14 +59,14 @@ class SoundManager {
 
   /**
    * 
-   * @param {{"background-song": {filepath: string, volume: number}}} soundData Contains our sound names and their file paths.
+   * @param {{"sound-name": {filepath: string, volume: number}}} soundData Contains our sound names and their file paths.
    */
   init(soundData) {
     try {
       window.AudioContext = window.AudioContext || window.webkitAudioContext;
       this.audioContext = new AudioContext();
-      this.volumeNode = this.audioContext.createGain();
-      this.volumeNode.connect(this.audioContext.destination);
+      this.globalVolumeNode = this.audioContext.createGain();
+      this.globalVolumeNode.connect(this.audioContext.destination);
 
       const FILE_PATH = "filepath";
       for (let soundName in soundData) {
@@ -119,7 +125,8 @@ class SoundManager {
       //  this alter's the volume that sound buffer source is playing at
       // Since we have all sounds running on the same volume, we define all
       //  loaded sounds to use volumeNode.
-      soundBufferSource.connect(this.volumeNode);
+      soundBufferSource.connect(this.globalVolumeNode);
+      soundBufferSource.connect(this.volumeNodes[songName]);
 
       soundBufferSource.start(0);
     }
@@ -158,40 +165,92 @@ class SoundManager {
     };
 
     this.globalVolume = clamp(newVolume);
-    this.volumeNode.gain.value = this.globalVolume;
+    this.globalVolumeNode.gain.value = this.globalVolume;
+  }
+
+  /**
+   * Changes the volume of the passed in song name,
+   * does nothing if the song name is of a song that doesn't exist.
+   * @param {string} songName Defines the name of song to be changed.
+   * @param {number} newVolume Defines the new volume to be set
+   */
+  changeSongVolume(songName, newVolume) {
+    function clamp(num) {
+      if (num > 1.0) {
+        return 1.0;
+      }
+      else if (num < 0.0) {
+        return 0.0;
+      }
+      else {
+        return num;
+      }
+    };
+
+    /** @type {GainNode} */
+    const volumeNode = this.volumeNodes[songName];
+    if (volumeNode !== undefined) {
+      volumeNode.gain.value = clamp(newVolume);
+    }
   }
 
   /**
    * Loads a sound file through a xml http request.
    * @param {string} name Defines the name of the song, will be used as it's ID by the sound manager.
    * @param {string} path Defines the file path to the song storage.
+   * @param {number} volume Defines the volume of this song (between 1.0 and 0.0)
    */
-  loadSoundFile(name, path) {
+  loadSoundFile(name, path, volume) {
     let xmlRequest = new XMLHttpRequest();
     xmlRequest.open("GET", path, true);
     xmlRequest.responseType = "arraybuffer";
-    xmlRequest.addEventListener("load", this.onBufferLoad.bind(this, name, xmlRequest), false);
+    xmlRequest.addEventListener("load", this.onBufferLoad.bind(this, name, volume, xmlRequest), false);
     xmlRequest.send();
   }
 
   /**
    * Called when a array of blob data is loaded.
    * @param {string} songName Defines the name of the song, to be used as id.
+   * @param {number} volume Defines the volume of the song, between 1.0 and 0.0.
    * @param {XMLHttpRequest} xmlRequest Defines the xml request that sent this request.
    * @param {Event} event Defines the response data.
    */
-  onBufferLoad(songName, xmlRequest, event) {
+  onBufferLoad(songName, volume, xmlRequest, event) {
     let arrayBuffer = xmlRequest.response;
 
-    this.audioContext.decodeAudioData(arrayBuffer, this.onDecodeSuccess.bind(this, songName), this.onDecodeFail.bind(this, songName));
+    this.audioContext.decodeAudioData(arrayBuffer, this.onDecodeSuccess.bind(this, songName, volume), this.onDecodeFail.bind(this, songName));
   }
 
   /**
    * Called when a audio buffer successfully decodes.
+   * @param {string} songName Defines the name of the song.
+   * @param {number} volume Defines the volume of the song, between 1.0 and 0.0.
    * @param {AudioBuffer} audioBuffer Defines the successfully decoded audio buffer.
    */
-  onDecodeSuccess(songName, audioBuffer) {
+  onDecodeSuccess(songName, volume, audioBuffer) {
+    /**
+     * @param {number} num
+     * @returns {number} clamped between 1.0 and 0.0
+     */
+    function clamp(num) {
+      if (num > 1.0) {
+        return 1.0;
+      }
+      else if (num < 0.0) {
+        return 0.0;
+      }
+      else {
+        return num;
+      }
+    };
+
     this.audioBuffers[songName] = audioBuffer;
+    const volumeNode = (this.volumeNodes[songName] !== undefined)
+        ? this.volumeNodes[songName]
+        : this.audioContext.createGain();
+    volumeNode.gain.value = clamp(volume);
+    this.volumeNodes[songName] = volumeNode;
+
     let soundQ = this.soundQueue;
     for (let index = soundQ.length - 1; index >= 0; --index) {
       let sound = soundQ[index];
