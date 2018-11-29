998bd4e7688052a35684cc16472fd5911709ed94
David O'Gorman <davemau51074@gmail.com>
2018-11-29 09:45:29 +0000

    Standard

--- src/scenes/game-scene/simple_block.js
+++ src/scenes/game-scene/simple_block.js
@@ -4,28 +4,26 @@
  * 
  */
 
- class Block{
+class Block {
 
-  constructor(sPosX, sPosY, sWidth, sHeight)
-  {
+  constructor(sPosX, sPosY, sWidth, sHeight) {
     this.position = {
-      x:sPosX,
-      y:sPosY
+      x: sPosX,
+      y: sPosY
     };
-  //  this.position =10;
-  //  this.position =10;
-    this.sWidth = 10; 
+    //  this.position =10;
+    //  this.position =10;
+    this.sWidth = 10;
     this.sHeight = 10;
 
-    this.floor ={x:800, y:300, w:300, h:60};
-    this.square ={x:300, y:300, w:100, h:100};
+    this.floor = { x: 800, y: 300, w: 300, h: 60 };
+    this.square = { x: 300, y: 300, w: 100, h: 100 };
     this
   }
 
-  update(dt)
-  {
-      this.sPosX = 300;
-      this.sPosY = 300;
+  update(dt) {
+    this.sPosX = 300;
+    this.sPosY = 300;
 
   }
 
@@ -35,7 +33,7 @@
     ctx.fillRect(this.floor.x, this.floor.y, this.floor.w, this.floor.h);
     ctx.fillRect(this.square.x, this.square.y, this.square.w, this.square.h);
     ctx.restore();
-    
+
   }
 
- }
+}
